7fc6aceca32b43f60d5e90587ba9f766c7be40dd
videogamesrockp <aster.deng@gmail.com>
2022-12-29 13:01:58 -0800

    Add report card block, report card armor, and the grade reporter.

---
+++ src/main/java/com/axd/stevenmod/items/Grade_Reporter.java
@@ -0,0 +1,26 @@
+package com.axd.stevenmod.items;
+
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+
+import java.util.List;
+
+public class Grade_Reporter extends Item {
+    public Grade_Reporter(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
+        if (hand == InteractionHand.MAIN_HAND && !level.isClientSide()) {
+            player.sendSystemMessage(Component.literal("Your grade is an F. Study harder you failure."));
+        }
+
+        return super.use(level, player, hand);
+    }
+}
--- src/main/java/com/axd/stevenmod/items/ModItems.java
+++ src/main/java/com/axd/stevenmod/items/ModItems.java
@@ -1,10 +1,11 @@
 package com.axd.stevenmod.items;
 
+import com.axd.stevenmod.ModArmorMaterial;
 import com.axd.stevenmod.StevenMod;
-import com.axd.stevenmod.blocks.ModBlocks;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.Item;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.item.*;
+import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
@@ -13,5 +14,31 @@ public class ModItems {
 
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, StevenMod.MODID);
 
+    public static final RegistryObject<Grade_Reporter> GRADE_REPORTER = ITEMS.register("grade_reporter",
+            () -> new Grade_Reporter(new Item.Properties().tab(CreativeModeTab.TAB_COMBAT)));
+
     public static final RegistryObject<Item> REPORT_CARD = ITEMS.register("report_card", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MATERIALS)));
+
+    public static final RegistryObject<ArmorItem> REPORT_CARD_HELMET = ITEMS.register("report_card_helmet",
+            () -> new ArmorItem(ArmorTiers.REPORT_CARD, EquipmentSlot.HEAD, new Item.Properties().tab(CreativeModeTab.TAB_COMBAT)));
+    public static final RegistryObject<ArmorItem> REPORT_CARD_CHESTPLATE = ITEMS.register("report_card_chestplate",
+            () -> new ArmorItem(ArmorTiers.REPORT_CARD, EquipmentSlot.CHEST, new Item.Properties().tab(CreativeModeTab.TAB_COMBAT)));
+    public static final RegistryObject<ArmorItem> REPORT_CARD_LEGGINGS = ITEMS.register("report_card_leggings",
+            () -> new ArmorItem(ArmorTiers.REPORT_CARD, EquipmentSlot.LEGS, new Item.Properties().tab(CreativeModeTab.TAB_COMBAT)));
+    public static final RegistryObject<ArmorItem> REPORT_CARD_BOOTS = ITEMS.register("report_card_boots",
+            () -> new ArmorItem(ArmorTiers.REPORT_CARD, EquipmentSlot.FEET, new Item.Properties().tab(CreativeModeTab.TAB_COMBAT)));
+
+
+    public static class ArmorTiers {
+        public static final ArmorMaterial REPORT_CARD = new ModArmorMaterial(
+                "report_card",
+                750,
+                new int[] { 20, 30, 40, 10 },
+                300,
+                SoundEvents.ARMOR_EQUIP_DIAMOND,
+                0.0f,
+                0.0f,
+                () -> Ingredient.of(ModItems.REPORT_CARD.get()));
+    }
+
 }
