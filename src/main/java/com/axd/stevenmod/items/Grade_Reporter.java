763045b2f68a081ccff4513d7e0cd76b0e95c12e
videogamesrockp <aster.deng@gmail.com>
2022-12-31 14:31:55 -0800

    small fix

--- src/main/java/com/axd/stevenmod/items/Grade_Reporter.java
+++ src/main/java/com/axd/stevenmod/items/Grade_Reporter.java
@@ -3,6 +3,7 @@ package com.axd.stevenmod.items;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
@@ -19,6 +20,7 @@ public class Grade_Reporter extends Item {
     public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
         if (hand == InteractionHand.MAIN_HAND && !level.isClientSide()) {
             player.sendSystemMessage(Component.literal("Your grade is an F. Study harder you failure."));
+            player.hurt(DamageSource.LIGHTNING_BOLT, 10f);
         }
 
         return super.use(level, player, hand);
